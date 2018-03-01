3d2bc5e24731b451acfc9c6ddabebdee353362e6
djemerson <djemerson@gmail.com>
2018-02-28 19:01:15 -0600

    added firebase info to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB8Y0Dy3-yPE35_ZOilH-omSe2ooMqLRfU",
+    authDomain: "employeetracker-c6938.firebaseapp.com",
+    databaseURL: "https://employeetracker-c6938.firebaseio.com",
+    projectId: "employeetracker-c6938",
+    storageBucket: "",
+    messagingSenderId: "20142424528"
+  };
+  firebase.initializeApp(config);
+</script>
